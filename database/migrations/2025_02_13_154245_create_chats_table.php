51856f972faf2643384442fbfe38dc084f08aa8f
ikolesnikov <ikolesnikov@dr-telemed.ru>
2025-02-17 15:43:02 +0300

    chats model

---
+++ database/migrations/2025_02_13_154245_create_chats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('slug')->nullable();
+            $table->integer('user1');
+            $table->integer('user2');
+            $table->json('options')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
